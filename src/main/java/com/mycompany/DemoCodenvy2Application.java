bfa9c94c9c50722a26bfb07a2f8b757395382940
ec2-user <ec2-user@ip-172-30-4-151.ec2.internal>
2014-12-18 13:57:22 +0000

    Import from Tomitribe Tribestream

---
+++ src/main/java/com/mycompany/DemoCodenvy2Application.java
@@ -0,0 +1,9 @@
+package com.mycompany;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/codenvy-demo2")
+public class DemoCodenvy2Application extends Application {
+    // no implementation here to let the container scans for resources
+}
